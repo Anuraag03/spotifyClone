07137d5ad56b13ce6b19de11f6ba41734a200389
Anuraag03 <f20211887@hyderabad.bits-pilani.ac.in>
2025-07-24 15:49:25 +0530

    updated vite-config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/spotifyClone/', // 👈 IMPORTANT: this fixes blank screen on GitHub Pages
+  plugins: [react(), tailwindcss()],
 })
